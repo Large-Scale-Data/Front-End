cde209ef64e354284eecadc1c2a00b1babbbd08b
Bethany <bethanysalazar2007@hotmail.com>
2022-05-04 13:43:20 -0500

    Added Transitions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Home } from './screens/Home';
+import { Home } from './screens/Home'
 import { ViewPortProvider } from './app/context'
-import './App.css';
-import { DynamicHeader } from './components/DynamicHeader';
-import { useEffect, useState } from 'react';
-import './components/SearchBar/style/searchBar.css';
-import axios from "axios";
-import { Box, Tab, Tabs } from '@mui/material';
-import { TabPanel } from './components/TabPanel';
-import { AllergyInfo } from './screens/AllergyInfo';
-import { AboutUs } from './screens/AboutUs';
+import './App.css'
+import { DynamicHeader } from './components/DynamicHeader'
+import { useEffect, useState } from 'react'
+import './components/SearchBar/style/searchBar.css'
+import axios from 'axios'
+import { Box, Tab, Tabs } from '@mui/material'
+import { TabPanel } from './components/TabPanel'
+import { AllergyInfo } from './screens/AllergyInfo'
+import { AboutUs } from './screens/AboutUs'
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
 
-
-function App() {
-  
+function App () {
   // const [data, setData] = useState({Data:[]});
   // useEffect(() =>{
   //   let x ;
@@ -20,48 +20,58 @@ function App() {
   //       .then(res => {
   //           x = res.x;
   //           setData({
-  //               Data : x    
+  //               Data : x
   //           });
   //       })
   //       .catch(err => {})
   // })
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setValue(newValue)
+  }
+  const theme = useTheme()
+  const handleChangeIndex = (index) => {
+    setValue(index);
   };
-
   return (
     <ViewPortProvider>
-    <DynamicHeader 
-      title={'Food Products Sold In the US, MX and UK'}
-      backgroundColor='#16A085'
-      color='white'
-      position='center'/>
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs 
-          value={value} 
-          onChange={handleChange}
-          textColor='secondary'
-          indicatorColor='secondary'
-          centered>
-          <Tab label="Home" />
-          <Tab label="Allergy Information"  />
-          <Tab label="About Us" />
-        </Tabs>
+      <DynamicHeader
+        title={'Food Products Sold In the US, MX and UK'}
+        backgroundColor='#16A085'
+        color='white'
+        position='center'
+      />
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor='secondary'
+            indicatorColor='secondary'
+            centered
+          >
+            <Tab label='Home' />
+            <Tab label='Allergy Information' />
+            <Tab label='About Us' />
+          </Tabs>
+        </Box>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}>
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <Home />
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <AllergyInfo />
+          </TabPanel>
+          <TabPanel value={value} index={2} dir={theme.direction}>
+            <AboutUs />
+          </TabPanel>
+        </SwipeableViews>
       </Box>
-      <TabPanel value={value} index={0}>
-        <Home/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <AllergyInfo/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <AboutUs/>
-      </TabPanel>
-    </Box>
     </ViewPortProvider>
   )
 }
 
-export default App;
+export default App
